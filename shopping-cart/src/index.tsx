0b34c107da197c477179c844dae3dc769fe60903
MadridBabajev <mbabajev2602@gmail.com>
2024-09-03 12:43:17 +0300

    Setup basic application structure

--- shopping-cart/src/index.tsx
+++ shopping-cart/src/index.tsx
@@ -1,19 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/site.css';
+import Root from './routes/Root';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import ErrorPage from "./routes/general/ErrorPage";
+import Home from "./routes/general/Home";
+import {Navigations} from "./types/strings/Navigations";
+import Register from "./routes/general/Register";
+import Login from "./routes/general/Login";
+import Catalog from "./routes/general/Catalog";
+import ItemDetails from "./routes/general/ItemDetails";
+import ShoppingCart from "./routes/authorized/ShoppingCart";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: Navigations.LOGIN,
+                element: <Login />,
+            },
+            {
+                path: Navigations.REGISTER,
+                element: <Register />,
+            },
+            {
+                path: Navigations.CATALOG,
+                element: <Catalog />,
+            },
+            {
+                path: Navigations.SHOP_CART,
+                element: <ShoppingCart />,
+            },
+            {
+                path: Navigations.ITEM_DETAILS,
+                element: <ItemDetails />
+            },
+        ]
+    }
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
+
 reportWebVitals();
---
+++ shopping-cart/src/types/strings/Navigations.ts
@@ -0,0 +1,15 @@
+export enum Navigations {
+    LOGIN = "/login",
+    REGISTER = "/register",
+    CATALOG = "/edit-profile",
+    SHOP_CART = "/my-availability",
+    ITEM_DETAILS = "/payment-methods",
+    BANKING_DETAILS = "/banking-details",
+    SUBJECTS = "/subjects",
+    TUTOR_SEARCH = "/tutors-search",
+    LESSON = "/lesson",
+    MY_LESSONS = "/my-lessons",
+    RESERVE_LESSON = "/reserve-lesson",
+    PAYMENT = "/payment",
+    INBOX = "/inbox"
+}
